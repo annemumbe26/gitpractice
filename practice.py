0e39c8602cf815d8901baf7b3954647bd5032f4c
devMunyi <samunyi90@gmail.com>
2025-04-07 09:13:34 +0300

    Function To Sum Two Numbers

--- practice.py
+++ practice.py
@@ -4,3 +4,8 @@ print("Data science Moringa")
 print("extra line of code")
 print("WRC 2025")
 print("Anne")
+
+def add_two_numbers(num1, num2):
+    return num1 + num2
+print(add_two_numbers(5, 10))
+print(add_two_numbers(20, 30))
